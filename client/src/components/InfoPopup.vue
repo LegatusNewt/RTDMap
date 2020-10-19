659fd4a91e3360309323b2706dbda156968ba1ab
LegatusNewt <kennywu1005@gmail.com>
2020-10-19 15:09:57 -0600

    No Commuter rail data. Added list of vehicles to route popup. Need to work on the design of Route popup info and better organize sidebar.

--- client/src/components/InfoPopup.vue
+++ client/src/components/InfoPopup.vue
@@ -1,10 +1,15 @@
 <template lang="html">
 <vs-popup id="info" style='position: absolute' class='holamundo' title="Route Info" :active.sync="active">
     <div>
-      <h2>{{`${route.route_long_name}`}}</h2>
-      <p>{{`${route.route_desc}`}}</p>
+      <h2>{{`${route ? route.route_long_name : 'No Route'}`}}</h2>
+      <p>{{`${route ? route.route_desc : 'No Route'}`}}</p>
       <p>{{`Data: ${JSON.stringify(route)}`}}</p>
-    </div>      
+      <ul>
+          <li v-for="v in vehicles" :key="v.id">
+            <span>{{v.vehicle.label}}</span>
+          </li>
+      </ul>
+    </div>
     <div>
 
     </div>
@@ -26,7 +31,14 @@ export default {
         }
     },
     route () {
-        return this.$store.state.rModule.infoData.route;
+      return this.$store.state.rModule.infoData.route || 'No Route';
+    },
+    vehicles () {
+      let rId = this.route ? this.route.route_id : null;
+      let vhs = this.$store.state.vehicles.filter( x => {
+        return x.trip && x.trip.routeId === rId;
+      });
+      return vhs;
     },
     trips: {
       get: function() {        
